bad2a91f3e8055e9acac6edd851fa3b3a9c787b7
JustWhxt <tibo.hrn@gmail.com>
2024-10-15 16:58:59 +0200

    Delete files that were of no use

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('Bienvenue sur FlexiTime !');
+
+  const handleChangeMessage = () => {
+    setMessage('Merci d\'utiliser FlexiTime !');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{message}</h1>
+        <button onClick={handleChangeMessage}>Changer le message</button>
       </header>
     </div>
   );
